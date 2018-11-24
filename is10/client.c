b9705811027e5a85f5bbdcc222f29f49b4f5fabd
Jeong Seok Hyeon <arcane1028@gmail.com>
2018-11-24 23:55:02 +0900

    Edit is10 client
    add comment and fix bug

--- is10/client.c
+++ is10/client.c
@@ -13,8 +13,8 @@ unsigned int des(unsigned char * buf, unsigned char * key, unsigned int msg_len,
     DES_cblock iv = {0,};
     unsigned int i, result, padding;
 
-    unsigned char block_in[BLOCK_SIZE] = {0,};
-    unsigned char block_out[BLOCK_SIZE] = {0,};
+    unsigned char block_in[MSG_SIZE] = {0,};
+    unsigned char block_out[MSG_SIZE] = {0,};
 
     DES_string_to_key(key, &des_key);
     DES_set_key_checked(&des_key, &des_ks);
@@ -22,21 +22,21 @@ unsigned int des(unsigned char * buf, unsigned char * key, unsigned int msg_len,
     memcpy(block_in, buf, msg_len);
     if (mode == 1)
     {
-        if(msg_len < BLOCK_SIZE)
+        if(msg_len < MSG_SIZE)
         {
-            padding = BLOCK_SIZE - msg_len;
-            block_in[BLOCK_SIZE-1]=padding;
+            padding = MSG_SIZE - msg_len;
+            block_in[MSG_SIZE-1]=padding;
         }
-        DES_ncbc_encrypt(block_in, block_out, BLOCK_SIZE, &des_ks, &iv, DES_ENCRYPT);
-		result = BLOCK_SIZE;
+        DES_ncbc_encrypt(block_in, block_out, MSG_SIZE, &des_ks, &iv, DES_ENCRYPT);
+		result = MSG_SIZE;
 	}
     else if(mode == 2)
     {
-        DES_ncbc_encrypt(block_in, block_out, BLOCK_SIZE, &des_ks, &iv, DES_DECRYPT);
-        padding = block_out[BLOCK_SIZE-1];
-        result = BLOCK_SIZE - padding;
+        DES_ncbc_encrypt(block_in, block_out, MSG_SIZE, &des_ks, &iv, DES_DECRYPT);
+        padding = block_out[MSG_SIZE-1];
+        result = MSG_SIZE - padding;
     }
-    memcpy(buf, block_out, BLOCK_SIZE);
+    memcpy(buf, block_out, MSG_SIZE);
 	return result;
 }
 void make_key(char * name){
@@ -45,25 +45,23 @@ void make_key(char * name){
     char *pri_key;
     char *pub_key;
 	char filename[64]={0,};
-	//filename = malloc(64);
+
     // 키생성
     RSA *keypair = RSA_generate_key(KEY_LENGTH, PUB_EXP, NULL, NULL);
 
     BIO *pri = BIO_new(BIO_s_mem());
     BIO *pub = BIO_new(BIO_s_mem());
-    // PEM 포멧으로 출력
+    
     PEM_write_bio_RSAPrivateKey(pri, keypair, NULL, NULL, 0, NULL, NULL);
     PEM_write_bio_RSAPublicKey(pub, keypair);
-
+	
     pri_len = BIO_pending(pri);
     pub_len = BIO_pending(pub);
-
     pri_key = malloc(pri_len +1);
     pub_key = malloc(pub_len+1);
-    // 문자열 출력
+
     BIO_read(pri, pri_key, pri_len);
     BIO_read(pub, pub_key, pub_len);
-
     pri_key[pri_len] = '\0';
     pub_key[pub_len] = '\0';
     // 파일 쓰기
@@ -91,7 +89,6 @@ void make_key(char * name){
     fclose(key_file);
     free(pri_key);
     free(pub_key);
-	//free(filename);
 }
 
 void * send_msg(void * arg)
@@ -160,13 +157,11 @@ int main(int argc, char *argv[])
 	//키 생성
 	memcpy(filepath, msg.name, 64);
 	strcat(filepath, "_pukey.pem");
-	printf("file name: %s\n", filepath);
 
 	if (access(filepath, F_OK) == -1){
 		make_key(msg.name);
 		printf("complete key\n");
 	}
-	printf("test\n");
 	if(strcmp("aaa", msg.name)==0){
 		printf("key : ");
 		scanf("%s", key);
@@ -176,17 +171,17 @@ int main(int argc, char *argv[])
 			printf("send error \n");
 		}
 		RSA *keypair = PEM_read_bio_RSAPublicKey(pub, NULL, NULL, NULL);
-		printf("\n test 1 \n");
+
 		unsigned char block_in[BLOCK_SIZE] = {0,};
         unsigned char block_out[BLOCK_SIZE] = {0,};
 		memcpy(block_in, key, KEY_SIZE);
-		printf("\n test 2 %s\n", block_in);
+
 		RSA_public_encrypt(strlen(key), block_in, block_out, keypair, RSA_PKCS1_PADDING);
-		printf("\n test 3 %s\n", block_out);
+
 		write(serv_sock, (void *)block_out, sizeof(block_out));
         
 	}else{
-		printf("bbb\n");
+
 		unsigned char block_in[BLOCK_SIZE] = {0,};
         unsigned char block_out[BLOCK_SIZE] = {0,};
 
@@ -198,13 +193,11 @@ int main(int argc, char *argv[])
 			printf("read error\n");
 		}
 		RSA *keypair = PEM_read_bio_RSAPrivateKey(pub, NULL, NULL, NULL);
-		printf("key test\n");
 
 		RSA_private_decrypt(BLOCK_SIZE, block_in, block_out, keypair, RSA_PKCS1_PADDING);
-		printf("key %s\n",block_out);
 
 		memcpy(key, block_out, KEY_SIZE);
-		printf("read key complete %s %d\n", key, sizeof(key));
+		printf("read key complete %s \n", key);
 	}
 	
 	pthread_create(&send_thread, NULL, send_msg, (void *)serv_sock);
