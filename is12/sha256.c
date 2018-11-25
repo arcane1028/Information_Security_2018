08f73fb274e49cdf4a29db7748ebf1947412d7d0
Jeong Seok Hyeon <arcane1028@gmail.com>
2018-11-25 18:20:04 +0900

    Edit is12
    hash complete

--- is12/sha256.c
+++ is12/sha256.c
@@ -14,61 +14,106 @@ int GetFileSize(FILE *fp)
 	fsize = ftell(fp);
 	fseek(fp, offset_bkup, SEEK_SET);
 
-	printf("File Size = %d\n", fsize);
+	//printf("File Size = %d\n", fsize);
 	return fsize;
 }
 
 int main(void){
-    int command;
-    printf("[1] Generate Hash [2] Hash Check [3] Quit\n");
-    scanf("%d", &command);
 
-    switch(command){
-        case 1:
-        {
-            char inputFileName[] = "hash.txt";
-            char outputFileName[] = "hash-sha256.txt";
-            FILE *input_FD;
-            FILE *output_FD;
+    while(1){
+        int command = 0;
+        printf("[1] Generate Hash [2] Hash Check [3] Quit\n");
+        scanf("%d", &command);
+        switch(command){
+            case 1:
+            {
+                char inputFileName[] = "hash.txt";
+                char outputFileName[] = "hash-sha256.txt";
+                FILE *input_FD;
+                FILE *output_FD;
 
-            input_FD = fopen(inputFileName, "rb");
-	        output_FD = fopen(outputFileName, "wb");
+                input_FD = fopen(inputFileName, "rb");
+                output_FD = fopen(outputFileName, "wb");
 
-            int fileSize = GetFileSize(input_FD);
-            char buff[fileSize];
-            char hash[32]={0,};
-            memset(buff, 0, fileSize);
+                int fileSize = GetFileSize(input_FD);
+                char buff[fileSize];
+                char hash[32]={0,};
+                memset(buff, 0, fileSize);
 
-            int len = fread(buff, sizeof(char), fileSize, input_FD);
-            printf("read length %d\n", len);
-            //buff[fileSize]='\0';
-            printf("%s\n", buff);
+                int len = fread(buff, sizeof(char), fileSize, input_FD);
+                //Hash
+                SHA256_CTX sha256;
+                SHA256_Init(&sha256);
+                SHA256_Update(&sha256, buff, len);
+                SHA256_Final(hash, &sha256);
 
-            SHA256_CTX sha256;
-            SHA256_Init(&sha256);
-            SHA256_Update(&sha256, buff, strlen(buff));
-            SHA256_Final(hash, &sha256);
+                char hex[strlen(hash)*2];
 
-            printf("hash length %d\n", strlen(hash));
+                for (int i = 0, j = 0; i < strlen(hash); ++i, j += 2)
+                    sprintf(hex + j, "%02x", hash[i] & 0xff);
 
-            char hex[strlen(hash)*2];
+                printf("%s\n", hex);
+                fwrite(hex, sizeof(char), 64,output_FD);
+                fclose(input_FD);
+                fclose(output_FD);
+                break;
+            }
+            case 2:
+            {
+                char originFileName[] = "hash-sha256.txt";
+                char inputFileName[64] = {0};
+                FILE *origin_FD;
+                FILE *input_FD;
 
-            for (int i = 0, j = 0; i < strlen(hash); ++i, j += 2)
-                sprintf(hex + j, "%02x", hash[i] & 0xff);
+                printf("Input File Name: ");
+                scanf("%s", inputFileName);
 
-            printf("%s %d\n", hex, strlen(hex));
-            fwrite(hex, sizeof(char), strlen(hex),output_FD);
-            fclose(input_FD);
-            fclose(output_FD);
-            break;
+                origin_FD = fopen(originFileName, "rb");
+                input_FD = fopen(inputFileName, "rb");
+
+                int originFileSize = GetFileSize(origin_FD);
+                int inputFileSize = GetFileSize(input_FD);
+                char originBuff[originFileSize+1];
+                char buff[inputFileSize];
+                char hash[32]={0,};
+                memset(originBuff, 0, originFileSize+1);
+                memset(buff, 0, inputFileSize);
+                
+                int len = fread(originBuff, sizeof(char), originFileSize, origin_FD);
+                originBuff[originFileSize] ='\0';// 문자열 끝 
+                printf("Original Hash : %s\n", originBuff);
+
+                len = fread(buff, sizeof(char), inputFileSize, input_FD);
+                //해쉬
+                SHA256_CTX sha256;
+                SHA256_Init(&sha256);
+                SHA256_Update(&sha256, buff, len);
+                SHA256_Final(hash, &sha256);
+
+                char hex[strlen(hash)*2+1];
+
+                for (int i = 0, j = 0; i < strlen(hash); ++i, j += 2)
+                    sprintf(hex + j, "%02x", hash[i] & 0xff);
+                hex[64] = '\0'; //문자열 끝
+                printf("Generated Hash: %s\n", hex);
+
+                if(strcmp(originBuff, hex)==0){
+                    printf("EQUAL\n");
+                }else{
+                    printf("DIFFERENT\n");
+                }
+
+                fclose(input_FD);
+                fclose(origin_FD);
+                break;
+            }
+                break;
+            case 3:
+                printf("QUIT\n");
+                exit(1);
+            default:
+                printf("command error\n");
         }
-        case 2:
-
-            break;
-        case 3:
-            exit(1);
-        default:
-            printf("command error\n");
     }
 
 }
